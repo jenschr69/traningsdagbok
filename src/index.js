c7e599dc26890f20963bac6b3e53e5a64e472e2c
Jens Christensen - Glodexa <jens@glodexa.com>
2025-05-14 15:06:36 +0200

    Creating load function for workouttypes (finally)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import express from 'express';
+import workouttypesRouter from './routes/workouttypes.js';
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
 app.use((req, res, next) => {
     console.log(req.method, req.url);
     next();
