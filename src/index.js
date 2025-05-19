f2df5b4527c1823a7bacc666f3e94e7a76502504
Jens Christensen - Glodexa <jens@glodexa.com>
2025-05-19 15:06:30 +0200

    Updating

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import workoutsRouter from './routes/workouts.js';
 import workouttypesRouter from './routes/workouttypes.js';
 import workoutsessionsRouter from './routes/workoutsessions.js';
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
     next();
 });
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
@@ -32,5 +32,5 @@ app.use('/workoutsessions', workoutsessionsRouter);
 app.use('/workouttypes', workouttypesRouter);
 
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:'+PORT);
+    console.log('Server is running on http://localhost:' + PORT);
 });
